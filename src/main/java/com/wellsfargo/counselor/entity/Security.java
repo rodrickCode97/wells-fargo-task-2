f632005676a82811236e4410c73d437aab683eef
rodrickcode97 <rodrickCode@gmail.com>
2024-05-20 22:08:47 -0500

    Finished creating data model

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -25,3 +25,41 @@ private Integer quantity
 @JoinColumn(name="portfolio_id")
 private Portfolio portfolio
 }
+
+// constructors
+
+public Security(String name, String catergory, Integer purchasePrice, Date purchaseDate, Integer quantity){
+    this.name = name;
+    this.catergory = catergory;
+    this.purchaseDate = purchaseDate;
+    this.purchasePrice = purchasePrice;
+    this.quantity = quantity;
+}
+
+public Lng getId(){
+    return id;
+}
+
+// Getters 
+public String getName(){ return name;}
+public void setName(String name){ this.name = name;}
+
+public String getCatergory(){ return catergory; }
+public void setCatergory(String catergory){ this.catergory = catergory}
+
+public Integer getPurchasePrice(){ return purchasePrice}
+public void setPurchasePrice(Integer purchasePrice){
+    this.purchasePrice = purchasePrice;
+}
+
+public Date getPurchaseDate(){return purchaseDate}
+public void setPurchaseDate(Date purchaseDate){
+    this.purchaseDate = purchaseDate;
+}
+
+public Integer getQuantity(){ return quantity}
+public void setQuantity(Integer qauntity){
+    this.qautity = quantity
+}
+
+// Setters
